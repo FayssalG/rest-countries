e4fa43f26fb43adc1840a8f00b86e1ef509171ac
FayssalG <fayssaldimawac123@gmail.com>
2023-01-27 15:19:01 +0100

    edit some padding inside CountryElement

--- src/components/CountryElement.jsx
+++ src/components/CountryElement.jsx
@@ -3,14 +3,14 @@ import { StyledLink } from "../styled-components/components"
 
 export default function CountryElement({country}){
     return(
-        <div  className="col-lg-3 ">
+        <div  className="col-lg-3">
             <StyledLink to={'/'+country.cca3} className="card nav-link px-0">
                 <img className="card-img-top img-fluid" loading="lazy" src={country.flags.svg} alt={country.cca3} />
-                <div className="card-body">
-                    <h2 className="card-title fw-bold fs-5">{country.name.common}</h2>
-                    <p className="card-text mb-0 fs-6"><span className="">Population {country.population}</span>  </p>
-                    <p className="card-text mb-0 fs-6"><span className="">Region {country.region}</span> </p>
-                    <p className="card-text mb-0 fs-6"><span className="">Capital {country.capital && country.capital[0]}</span> </p>
+                <div className="card-body py-4">
+                    <h2 className="card-title fw-bolder fs-5 ">{country.name.common}</h2>
+                    <p className="card-text mb-0 fs-6"><span className="fw-semibold">Population</span>: {country.population}</p>
+                    <p className="card-text mb-0 fs-6"><span className="fw-semibold">Region</span>: {country.region} </p>
+                    <p className="card-text mb-0 fs-6"><span className="fw-semibold">Capital</span>: {country.capital && country.capital[0]} </p>
                 </div>
             </StyledLink>
         </div>
